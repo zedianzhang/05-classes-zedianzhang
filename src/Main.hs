bcda8cb5b277599531e06956e8dda0d095c0a90e
zedianzhang <48851268+zedianzhang@users.noreply.github.com>
2021-11-27 17:49:02 -0800

    Initial commit

---
+++ src/Main.hs
@@ -0,0 +1,19 @@
+import qualified Language.Nano.Types  as Nano
+import qualified Language.Nano.Eval   as Nano
+import           Language.Nano.Repl
+import           Text.Printf
+import           GHC.IO.Encoding
+
+main :: IO ()                             
+main = do
+  setLocaleEncoding utf8
+  error "TBD: main"
+
+--------------------------------------------------------------------------------
+-- | Some useful functions 
+--------------------------------------------------------------------------------
+-- putStr   :: String -> IO ()
+-- hFlush   :: 
+-- putStrLn :: String -> IO ()
+-- getLine  :: IO String 
+
